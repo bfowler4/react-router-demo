90b80158cad64915ea6ee31b7be19af6e623ca1d
Brandon Fowler <bmfowler4@gmail.com>
2018-03-02 11:54:02 -1000

    added clickable links in planet page and redirects

--- src/Planet.js
+++ src/Planet.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 class Planet extends Component {
   constructor(props) {
@@ -27,6 +28,11 @@ class Planet extends Component {
 
   render() {
     console.log('render', this.state)
+    if (this.state.detail === 'Not found') {
+      return (
+        <Redirect to='/planets' />
+      );
+    }
     const {
       climate,
       diameter,
